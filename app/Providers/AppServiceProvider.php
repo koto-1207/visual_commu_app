b344f70047957062c295d1112d460f95c09f9989
koto-1207 <0429koto@gmail.com>
2025-11-01 18:15:33 +0900

    AppServiceProviderの修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\UrlGenerator;
+use Livewire\Volt\Volt;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Mount Volt early in the application lifecycle
+        Volt::mount([
+            resource_path('views/livewire'),
+        ]);
     }
 
     /**
